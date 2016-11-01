f1defadc1ba27fb106fefb3988841baf55381763
snam4 <snam4@saic.edu>
2016-11-01 11:08:04 -0400

    Update coinCount.cs

--- preview/w10-animation/coinCount.cs
+++ preview/w10-animation/coinCount.cs
@@ -1,18 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+//Add this to the character
 public class coinCount : MonoBehaviour {
 
 	public int score;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
 	void OnCollisionEnter(Collision other){
 		if (other.collider.tag == "coin") {
