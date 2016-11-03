070ea1b6cbe8dc91ae55b59e7f0f2dc936d2b0e4
snam4 <snam4@saic.edu>
2016-11-03 16:14:27 -0400

    w10-animation review

---
+++ w10/w10_doorOpenMecanim.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class w10_doorOpenMecanim : MonoBehaviour {
+	Animator anim;
+
+	void Start(){
+		anim = GetComponent<Animator> ();
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			anim.SetBool ("open", true);
+			anim.SetBool ("close", false);
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.tag == "Player"){
+			anim.SetBool ("open", false);
+			anim.SetBool ("close", true);
+		}
+	}
+}
