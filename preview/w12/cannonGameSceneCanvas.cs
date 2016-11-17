9e8e9c322eb61b1dbf6bee4bc667c4eea4d0fffb
snam4 <snam4@saic.edu>
2016-11-17 12:54:30 -0500

    w12-preview-thursday

---
+++ preview/w12/cannonGameSceneCanvas.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class cannonGameSceneCanvas : MonoBehaviour {
+	public float eHealth;
+	public Slider eHealthBar;
+	public Text eHealthText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		eHealth = enemyHealth.eHealth;
+		eHealthBar.value = eHealth;
+		eHealthText.text = "Health : " + eHealth;
+	}
+}
---
+++ preview/w12/coinInstantiate.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coinInstantiate : MonoBehaviour {
+	public GameObject coinPrefab;
+	GameObject coinClone;
+	public Transform pos;
+	float timer = 0.0f;
+	Vector3 spawn_pos;
+	float x,y,z;
+
+	void spawn_coin(){
+		x = pos.position.x + Random.Range (-10.0f, 10.0f);
+		y = pos.position.y;
+		z = pos.position.z + Random.Range (-10.0f, 10.0f);
+		spawn_pos = new Vector3 (x, y, z);
+		coinClone = Instantiate (coinPrefab, spawn_pos, coinPrefab.transform.rotation) as GameObject;
+	}
+
+	// Use this for initialization
+	void Start () {
+		//coinClone = Instantiate (coinPrefab, coinPrefab.transform.position, coinPrefab.transform.rotation) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > 3) {
+			spawn_coin ();
+			timer = 0.0f;
+		}
+	}
+
+	
+}
