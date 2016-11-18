4cdcf82ea639f888e3524fd4a1016bde277f3498
snam4 <snam4@saic.edu>
2016-11-17 20:47:25 -0500

    w12-preview

---
+++ preview/w12/coinCount.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coinCount : MonoBehaviour {
+
+	public static int score; //access to this score when using trigger (cannot detect a collision)
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print (score);
+	}
+
+//	void OnCollisionEnter(Collision other){
+//		if (other.collider.tag == "coin") {
+//			score++;
+//		}
+//	}
+}
