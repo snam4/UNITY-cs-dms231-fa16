bd1516f394eac31595ac277e8ad15f04fddeab6f
snam4 <snam4@saic.edu>
2016-11-17 20:28:12 -0500

    w12-preview

---
+++ preview/w12/DestoryCoin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+//Destroy : https://unity3d.com/learn/tutorials/topics/scripting/destroy
+public class DestoryCoin : MonoBehaviour {
+	public int cCount;
+
+	void OnTriggerEnter(Collider other){
+		//Debug.Log ("collide");
+		Destroy (gameObject);
+		coinCount.score++;
+	}
+
+//COLLISION METHOD - PHISICS PROBLEM
+//	void OnCollisionEnter(Collision other){
+//		Debug.Log ("collide");
+//		Destroy (gameObject);
+//	}
+}
