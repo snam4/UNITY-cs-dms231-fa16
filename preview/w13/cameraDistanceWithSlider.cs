4ae627dd50ab41fbeb2e0c2a571df0d09a082d3d
snam4 <snam4@saic.edu>
2016-11-22 11:20:49 -0500

    w13-preview

---
+++ preview/w13/cameraDistanceWithSlider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraDistanceWithSlider : MonoBehaviour {
+	float sp =10f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up, sp * Time.deltaTime, Space.World);
+	}
+
+	public void SpeedControl(float newSp){
+		sp = newSp*30f;
+	}
+}
