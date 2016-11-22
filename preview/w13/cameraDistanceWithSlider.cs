4def92271abb498753bdd84af74ade77b26a244b
snam4 <snam4@saic.edu>
2016-11-22 12:43:50 -0500

    Update cameraDistanceWithSlider.cs

--- preview/w13/cameraDistanceWithSlider.cs
+++ preview/w13/cameraDistanceWithSlider.cs
@@ -2,19 +2,25 @@
 using System.Collections;
 
 public class cameraDistanceWithSlider : MonoBehaviour {
-	float sp =10f;
+	float sp =0.0f;
 
 	// Use this for initialization
 	void Start () {
 
 	}
 
-	// Update is called once per frame
+	// Camera Rotation Control
+	//void Update () {
+	//	transform.Rotate (Vector3.up, sp * Time.deltaTime, Space.World);
+	//}
+	//public void SpeedControl(float newSp){
+	//	sp = newSp*30f;
+	//}
+	//Camera 
 	void Update () {
-		transform.Rotate (Vector3.up, sp * Time.deltaTime, Space.World);
+		transform.Translate(Vector3.up*sp, Space.World);
 	}
-
 	public void SpeedControl(float newSp){
-		sp = newSp*30f;
+		sp = newSp * Time.deltaTime;
 	}
 }
